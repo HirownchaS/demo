16880ad1f08642de67dfd920d243b34a4df0920a
HirownchaS <hirowncha@gmail.com>
2025-10-23 16:33:11 +0530

    update db

--- src/main/java/db/DBUtil.java
+++ src/main/java/db/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL="jdbc:mysql://127.0.0.1:3306/trainee";
     private static final String USERNAME="root";
-    private static final String PASSWORD="password";
+    private static final String PASSWORD="";
 
 
     static{
